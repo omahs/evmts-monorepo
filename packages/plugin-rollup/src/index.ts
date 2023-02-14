50b9921feb212c4514e6a674dbf8a77cd21407fa
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-14 15:44:44 -0800

    :sparkles: add boilerplate for rollup plugin

--- packages/plugin-rollup/src/index.ts
+++ packages/plugin-rollup/src/index.ts
@@ -1,1 +1,27 @@
-export const plugin = 'TODO'
+import type { Plugin } from 'rollup'
+import { z } from 'zod'
+
+const optionsValidator = z.object({
+  forgeExecutable: z.string().optional(),
+})
+
+type Options = z.infer<typeof optionsValidator>
+
+export default function envTsPluginRollup(options: Options = {}): Plugin {
+  optionsValidator.parse(options)
+  return {
+    name: '@evmts/plugin-rollup',
+    resolveId(id, importer) {
+      if (!id.endsWith('.sol')) {
+        return
+      }
+      console.log('new solidity file', { id, importer })
+    },
+    load(id) {
+      if (!id.endsWith('.sol')) {
+        return
+      }
+      console.log('loading solidity file', { id })
+    },
+  }
+}
