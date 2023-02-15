9e02e797ab5c3c031682e1e199d39fe33a3173c6
Will Cory <willcory@Wills-MacBook-Pro.local>
2023-02-14 16:03:15 -0800

    :sparkles: add forge options

--- packages/plugin-rollup/src/index.ts
+++ packages/plugin-rollup/src/index.ts
@@ -2,13 +2,22 @@ import type { Plugin } from 'rollup'
 import { z } from 'zod'
 
 const optionsValidator = z.object({
-  forgeExecutable: z.string().optional(),
+  forgeExecutable: z
+    .string()
+    .optional()
+    .default('forge')
+    .describe('path to forge executable'),
+  projectRoot: z
+    .string()
+    .optional()
+    .default(process.cwd())
+    .describe('path to project root'),
 })
 
-type Options = z.infer<typeof optionsValidator>
+type Options = Partial<z.infer<typeof optionsValidator>>
 
 export default function envTsPluginRollup(options: Options = {}): Plugin {
-  optionsValidator.parse(options)
+  const { forgeExecutable, projectRoot } = optionsValidator.parse(options)
   return {
     name: '@evmts/plugin-rollup',
     resolveId(id, importer) {
@@ -22,6 +31,11 @@ export default function envTsPluginRollup(options: Options = {}): Plugin {
         return
       }
       console.log('loading solidity file', { id })
+
+      return `
+        export const abi = ${JSON.stringify(abi)}
+        export const bytecode = ${JSON.stringify(bytecode)}
+      `
     },
   }
 }
